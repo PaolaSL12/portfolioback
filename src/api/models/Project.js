4496d5713a33b48dd875b9f6db9a37667777c113
PaolaSL12 <paola.sanchezl12@gmail.com>
2025-01-23 09:51:22 +0100

    change model proyects

--- src/api/models/Project.js
+++ src/api/models/Project.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const projectSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: [
+      {
+        titleDesc: { type: String, required: true },
+        desc: [{ type: String, required: true }]
+      }
+    ],
     technologies: { type: String, required: true },
     url: { type: String, required: true },
     imageUrl: { type: String, required: true },
